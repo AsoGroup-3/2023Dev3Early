bbcd91558161959f44f343f6ba38431b373375bd
tarotarotarot <tarotou1018@gmail.com>
2023-06-01 11:20:30 +0900

    テーブル作成中

---
+++ 03_DB設計書/DBtable.sql
@@ -0,0 +1,24 @@
+-- スレッド
+CREATE TABLE threads(
+    thread_id    INT(16)      AUTO_INCREMENT NOT NULL,
+    thread_name  VARCHAR(512)                NOT NULL,
+    thread_bytes INT(16)                     NOT NULL,
+    PRIMARY KEY(thread_id));
+
+-- ユーザー
+CREATE TABLE users(
+    user_id     INT         NOT NULL,
+    user_name   VARCHAR(64) NOT NULL,
+    create_date DATE        NOT NULL,
+    PRIMARY KEY(user_id));
+
+-- スレッドコメント
+CREATE TABLE thread_comments(
+    thread_comment_id INT           NOT NULL,
+    comment           VARCHAR(1024) NOT NULL,
+    create_at         DATE          NOT NULL,
+    user_id           INT           NOT NULL,
+    thread_id         INT           NOT NULL,
+    PRIMARY KEY(thread_comment_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (thread_id) REFERENCES threads(thread_id));
