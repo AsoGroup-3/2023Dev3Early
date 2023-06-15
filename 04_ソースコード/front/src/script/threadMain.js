7ad9445fd89eebcb6a491ab984d3d5e63bf9e327
tarotarotarot <tarotou1018@gmail.com>
2023-06-15 12:42:14 +0900

    コメント表示機能作成中

--- 04_ソースコード/front/src/script/threadMain.js
+++ 04_ソースコード/front/src/script/threadMain.js
@@ -1,41 +1,19 @@
-// const vm = new Vue({
-//     el: '#app',
-//     data: {
-//         //初期値
-//         results: {},
-//     },
-//     //ページが読み込まれた時に動く処理
-//     mounted() {
-//         axios
-//                 //timestamp=${new Date().getTime()}を入れることで毎回違うアドレスで検索が出来るから以前のキャッシュを読み込まない
-//                 .get("http://localhost/web/2023Dev3Early/04_ソースコード/back/src/threadCommentDisplay.php")
-//                 .then((response) => (this.results = response.data))
-//                 .catch((error) => console.log(error));
-//     },
-//     methods: {
-//     },
-
-//     computed: {
-//     }
-
-// });
-
 const vm = new Vue({
     el: '#app',
     data: {
-      results: {}, // 初期値
+      comments: {}, // 初期値
     },
     mounted() {
-      this.fetchResults(); // ページが読み込まれた時にデータを取得する関数を呼び出す
+      this.fetchComments(); // ページが読み込まれた時にデータを取得する関数を呼び出す
     },
     methods: {
-      fetchResults() {
+      fetchComments() {
         const url = "http://localhost/web/2023Dev3Early/04_ソースコード/back/src/threadCommentDisplay.php";
         const timestamp = new Date().getTime(); // 毎回違うアドレスで検索するためのタイムスタンプ
         axios
           .get(`${url}?timestamp=${timestamp}`)
           .then((response) => {
-            this.results = response.data; // 取得したデータをresultsに代入
+            this.comments = response.data; // 取得したデータをcommentsに代入
           })
           .catch((error) => {
             console.log(error); // エラーが発生した場合はエラーメッセージをコンソールに表示
