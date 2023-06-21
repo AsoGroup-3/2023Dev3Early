446141fecf51b2c3f21c6a7fd2d7a7e45c7b031c
taketakebamboo <takeuguisu@gmail.com>
2023-06-21 12:08:44 +0900

    [fix] ログインユーザーをwebuserに変更

--- 04_ソースコード/back/app/dao/connectDAO.php
+++ 04_ソースコード/back/app/dao/connectDAO.php
@@ -1,6 +1,6 @@
 <?php
 function dbconnect()
 {
-    $pdo = new PDO('mysql:host=localhost;dbname=rordb;charset=utf8', 'root', '');
+    $pdo = new PDO('mysql:host=localhost;dbname=rordb;charset=utf8', 'webuser', 'abccsd2');
     return $pdo;
 }
