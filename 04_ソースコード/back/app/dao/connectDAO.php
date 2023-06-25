9a8de53627da86f7ac9e80984477890ff0dafd92
tarotarotarot <tarotou1018@gmail.com>
2023-06-25 23:36:23 +0900

    ディレクトリ構造・ファイル拡張子変更に伴うパス変更

--- 04_ソースコード/back/app/dao/connectDAO.php
+++ 04_ソースコード/back/app/dao/connectDAO.php
@@ -2,5 +2,6 @@
 function dbconnect()
 {
     $pdo = new PDO('mysql:host=localhost;dbname=rordb;charset=utf8', 'webuser', 'abccsd2');
+    // $pdo = new PDO('mysql:host=localhost;dbname=rordb;charset=utf8', 'user', 'abccsd2');
     return $pdo;
 }
