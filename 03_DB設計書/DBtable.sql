24700a5ade7f24f5debf7c1bcc36f2ea5585598c
tarotarotarot <tarotou1018@gmail.com>
2023-06-30 10:49:34 +0900

    テーブル変更

--- 03_DB設計書/DBtable.sql
+++ 03_DB設計書/DBtable.sql
@@ -1,14 +1,15 @@
 CREATE TABLE threads(
-    thread_id    INT          NOT NULL AUTO_INCREMENT,
-    thread_name  VARCHAR(512) NOT NULL,
-    thread_bytes INT ,
+    thread_id          INT          NOT NULL AUTO_INCREMENT,
+    thread_name        VARCHAR(512) NOT NULL,
+    thread_bytes       INT ,
+    thread_create_date DATETIME     NOT NULL,
     PRIMARY KEY (thread_id)
-    );
+);
 
 CREATE TABLE users(
-    user_id     VARCHAR(64) NOT NULL,
-    user_name   VARCHAR(64) NULL,
-    create_date DATETIME    NOT NULL,
+    user_id          INT         NOT NULL AUTO_INCREMENT,
+    user_name        VARCHAR(64) NULL,
+    user_create_date DATETIME    NOT NULL,
     PRIMARY KEY (user_id)
 );
 
@@ -16,17 +17,17 @@ CREATE TABLE thread_comments(
     thread_comment_id INT           NOT NULL AUTO_INCREMENT,
     comments          VARCHAR(1024) NOT NULL,
     create_at         DATETIME      NOT NULL,
-    user_id           VARCHAR(64)   NOt NULL,
+    user_id           INT           NOT NULL,
+    user_name         VARCHAR(64)   NULL,
     thread_id         INT           NOT NULL,
     PRIMARY KEY (thread_comment_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
     FOREIGN KEY (thread_id) REFERENCES threads(thread_id)
 );
 
 CREATE TABLE rings (
     ring_id         INT         NOT NULL AUTO_INCREMENT,
-    create_user     VARCHAR(64) NOT NULL,
-    invitation_user VARCHAR(64) NOT NULL,
+    create_user     INT         NOT NULL,
+    invitation_user INT         NOT NULL,
     thread_id       INT         NOT NULL,
     PRIMARY KEY (ring_id) ,
     FOREIGN KEY (create_user) REFERENCES users(user_id),
@@ -38,7 +39,7 @@ CREATE TABLE ring_comments(
     ring_comment_id INT          NOT NULL AUTO_INCREMENT,
     ring_comment    VARCHAR(512) NOT NULL,
     create_date     DATETIME     NOT NULL,
-    user_id         VARCHAR(64)  NOT NULL,
+    user_id         INT          NOT NULL,
     ring_id         INT          NOT NULL,
     PRIMARY KEY (ring_comment_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id),
---
+++ 03_DB設計書/DropTable.sql
@@ -0,0 +1,5 @@
+DROP TABLE ring_comments;
+DROP TABLE thread_comments;
+DROP TABLE rings;
+DROP TABLE threads;
+DROP TABLE users;
