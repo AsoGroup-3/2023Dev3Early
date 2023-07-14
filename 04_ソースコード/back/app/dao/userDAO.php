06bfc3ca16e60184822b2c0ff2edfdfd968ef7b6
tarotarotarot <tarotou1018@gmail.com>
2023-07-14 11:22:51 +0900

    関数の移動

---
+++ 04_ソースコード/back/app/dao/userDAO.php
@@ -0,0 +1,70 @@
+<?php
+
+//JSON形式で返却すること、文字形式がUTF-8だということの宣言
+
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: application/json; charset=UTF-8');
+require_once 'connectDAO.php';
+require_once 'versatility.php';
+
+class user_main
+{
+    //ユーザー名登録
+    function create_user($user_id,$user_name){
+        $pdo = dbconnect();
+        $sql = 'INSERT INTO users VALUE(?,?,?)';
+        $ps = $pdo->prepare($sql);
+
+        if($user_name==""){
+            $user_name="風吹けば名無し";
+        }
+        $ps->bindValue(1, $user_id, PDO::PARAM_STR);
+        $ps->bindValue(2, $user_name, PDO::PARAM_STR);
+        $ps->bindValue(3, date("Y/m/d H:i:s"), PDO::PARAM_STR);
+
+        $ps->execute();
+    }
+
+    //ユーザーIDからユーザー名取得
+    function get_user_name($user_id)
+    {
+        $pdo = dbconnect();
+        // 修正箇所
+        $sql = 'SELECT user_name FROM users WHERE user_id = ?';
+        $ps = $pdo->prepare($sql);
+        $ps->bindValue(1, $user_id, PDO::PARAM_STR);
+        $ps->execute();
+        $user_name = $ps->fetch();
+
+        $user_name = $user_name["user_name"];
+        return $user_name;
+    }
+
+    //ユーザー名更新
+    function update_user_name($user_id,$user_name){
+        $pdo = dbconnect();
+        $sql = 'UPDATE users SET user_name = ? WHERE user_id = ?';
+        $ps = $pdo->prepare($sql);
+
+        $ps->bindValue(1, $user_name, PDO::PARAM_STR);
+        $ps->bindValue(2, $user_id, PDO::PARAM_STR);
+
+        $ps->execute();
+    }
+
+    //セッションで保持しているIDの作成日が一日経過しているかを確認
+    function update_user_id($user_id){
+        $pdo = dbconnect();
+        $sql = 'SELECT create_at FROM users WHERE user_id = ?';
+        $ps = $pdo->prepare($sql);
+
+        $ps->bindValue(1, $user_id, PDO::PARAM_STR);
+
+        $ps->execute();
+
+        $user_create_day = $ps->fetch();
+
+        return checkSession($user_create_day["create_at"]);
+    }
+}
+?>
--- 04_ソースコード/back/src/commentCreate.php
+++ 04_ソースコード/back/src/commentCreate.php
@@ -1,19 +1,21 @@
 <?php
 session_start();
+require_once "../app/dao/userDAO.php";
 require_once "../app/dao/threadDAO.php";
-$dbm = new thread_main();
+$dbm_t = new thread_main();
+$dbm_u = new user_main();
 
 try{
 
     if (checkSession('user')) {
         $userData = $_SESSION['user'];
     
-        if ($userData['user_name'] != null && $_POST['user_name'] == $dbm->get_user_name($userData['user_id'])) {
-            $dbm->write_in_comment($userData['user_id'], $_POST["comment_detail"], $_POST["thread_id"]);
+        if ($userData['user_name'] != null && $_POST['user_name'] == $dbm_u->get_user_name($userData['user_id'])) {
+            $dbm_t->write_in_comment($userData['user_id'], $_POST["comment_detail"], $_POST["thread_id"]);
         } else {
-            $dbm->update_user_name($userData['user_id'], $_POST['user_name']);
+            $dbm_u->update_user_name($userData['user_id'], $_POST['user_name']);
             $_SESSION['user']['user_name'] = $_POST['user_name'];
-            $dbm->write_in_comment($userData['user_id'], $_POST["comment_detail"], $_POST["thread_id"]);
+            $dbm_t->write_in_comment($userData['user_id'], $_POST["comment_detail"], $_POST["thread_id"]);
         }
     } else {
         $userId = create_user_id();
@@ -23,8 +25,8 @@ try{
             'user_name' => $userName,
         ];
     
-        $dbm->create_user($userId, $userName);
-        $dbm->write_in_comment($userId, $_POST["comment_detail"], $_POST["thread_id"]);
+        $dbm_u->create_user($userId, $userName);
+        $dbm_t->write_in_comment($userId, $_POST["comment_detail"], $_POST["thread_id"]);
     }
 }catch(Exception $e){
     echo $e->getMessage();
