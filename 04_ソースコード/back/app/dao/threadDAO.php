15958063b5f1b448f75b7720124ef61117eea8f3
tarotarotarot <tarotou1018@gmail.com>
2023-07-19 20:55:59 +0900

    スレッド作成後、作成スレッドに遷移する機能完成

--- 04_ソースコード/back/app/dao/threadDAO.php
+++ 04_ソースコード/back/app/dao/threadDAO.php
@@ -55,7 +55,7 @@ class thread_main
         print $json_array;
     }
 
-    //スレッド取得昨日
+    //スレッド取得機能
     function thread_get()
     {
         $pdo = dbconnect();
@@ -105,7 +105,6 @@ class thread_main
         $pdo = dbconnect();
         $sql = 'INSERT INTO thread_comments VALUE(null,?,?,?,?,?)';
         $ps = $pdo->prepare($sql);
-        // $ps->bindValue(1, create_user_id(), PDO::PARAM_STR);
         $ps->bindValue(1, $comment_detail, PDO::PARAM_STR);
         $ps->bindValue(2, date("Y/m/d H:i:s"), PDO::PARAM_STR);
         $ps->bindValue(3, $user_id, PDO::PARAM_STR);
@@ -114,5 +113,38 @@ class thread_main
 
         $ps->execute();
     }
+
+    //スレッド検索・取得機能
+    function serch_thread_get($keyword)
+    {
+        $pdo = dbconnect();
+        $sql = 'SELECT * FROM threads WHERE thread_name LIKE ?';
+        $ps = $pdo->prepare($sql);
+        $ps->bindValue(1, "%".$keyword."%", PDO::PARAM_STR);
+        $ps->execute();
+        $thread = $ps->fetchAll();
+
+        $thread_data = array();
+
+        $currentDateTime = date('Y-m-d H:i:s');
+
+        //データベースから持ってきたデータをforeachを利用してデータの数だけ$thr_dataに追加している
+        foreach ($thread as $row) {
+
+            array_push($thread_data, array(
+                'thread_id' => $row['thread_id'],
+                'thread_name' => $row['thread_name'],
+                'thread_bytes' => $row['thread_bytes'],
+                'thread_create_date' => $row['thread_create_date'],
+                'created_date_time' => getDateDiff($currentDateTime, $row['thread_create_date']),
+                'thread_url' => 'http://localhost/2023Dev3Early/04_%E3%82%BD%E3%83%BC%E3%82%B9%E3%82%B3%E3%83%BC%E3%83%89/front/src/threadMain.php' . '?thread_id=' . $row['thread_id'],
+            ));
+        }
+        //arrayの中身をJSON形式に変換している
+        $json_array = json_encode($thread_data);
+
+        print $json_array;
+    }
+    
 
 }
