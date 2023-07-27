ac24c36d8d64bff328688c1ac6899e52903b42f0
taketakebamboo <takeuguisu@gmail.com>
2023-07-27 21:25:35 +0900

    [fix] パスワードを修正

--- 04_ソースコード/back/app/dao/connectDAO.php
+++ 04_ソースコード/back/app/dao/connectDAO.php
@@ -1,7 +1,7 @@
 <?php
 function dbconnect()
 {
-    $pdo = new PDO('mysql:host=mysql217.phy.lolipop.lan;dbname=LAA1418145-rordb;charset=utf8', 'LAA1418145', 'teamror');
+    $pdo = new PDO('mysql:host=mysql217.phy.lolipop.lan;dbname=LAA1418145-rordb;charset=utf8', 'LAA1418145', 'tesmror');
 
     return $pdo;
 }
